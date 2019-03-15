84faa2eaca9e9730f97e55146004c974571ad689
tdar31 <t.dardani8@gmail.com>
2019-03-15 00:58:55 -0700

    fds

--- client/src/components/SearchBar/index.js
+++ client/src/components/SearchBar/index.js
@@ -33,7 +33,7 @@ function SearchBar({ onClick, value, onChange, queryUser, region }) {
         </span>
       </p> */}
       <p className="control">
-        <Link to="prof" params={{ username: "goldenglue" }}>
+        <Link to="/summoner/goldenglue/NA" params={{ username: "goldenglue" }}>
           <a className="button is-dark">Search</a>
         </Link>
         <a className="button is-success" onClick={onClick}>
