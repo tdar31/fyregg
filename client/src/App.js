d15bb25fd8f8c39e5bca0d8cdb7ade0db78658e2
tdar31 <t.dardani8@gmail.com>
2019-03-15 00:51:58 -0700

    fds

--- client/src/App.js
+++ client/src/App.js
@@ -14,9 +14,22 @@ function App() {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/match" component={Matchpage} />
-          <Route exact path="/summoner/:username/:region" component={Profilepage} />
-          <Route exact path="/summoner/:username/:region/:theme" component={Profilepage} />
-          <Route exact path="/summoner/:username/:region/:matchData" component={Profilepage} />
+          <Route
+            exact
+            name="prof"
+            path="/summoner/:username/:region"
+            component={Profilepage}
+          />
+          <Route
+            exact
+            path="/summoner/:username/:region/:theme"
+            component={Profilepage}
+          />
+          <Route
+            exact
+            path="/summoner/:username/:region/:matchData"
+            component={Profilepage}
+          />
           <Route exact path="/books" component={Books} />
           <Route exact path="/books/:id" component={Detail} />
           <Route component={NoMatch} />
