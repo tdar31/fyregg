aa515d1aa4d27a86669de133ce56e9ac2fb73bd1
tdar31 <t.dardani8@gmail.com>
2019-03-14 23:31:08 -0700

    afd

--- client/src/pages/NoMatch.js
+++ client/src/pages/NoMatch.js
@@ -1,23 +1,30 @@
 import React from "react";
 import { Col, Row, Container } from "../components/Grid";
-import Jumbotron from "../components/Jumbotron";
+import ProfileContainer from "../components/ProfileContainer";
+import Nav from "../components/Nav";
+import ProfileBody from "../components/ProfileBody";
 
 function NoMatch() {
   return (
-    <Container fluid>
-      <Row>
-        <Col size="md-12">
-          <Jumbotron>
-            <h1>404 Page Not Found</h1>
-            <h1>
-              <span role="img" aria-label="Face With Rolling Eyes Emoji">
-                🙄
-              </span>
-            </h1>
-          </Jumbotron>
-        </Col>
-      </Row>
-    </Container>
+    <div>
+      <ProfileContainer>
+        <Nav />
+        <ProfileBody>
+          <div className="box">
+            <article className="media">
+              <div className="media-content userNotFoundContent">
+                <p className="userNotFoundText">
+                  404 page not found D:
+                  <br />
+                </p>
+                <img className="notFoundIMG" src="/images/splash/Amumu_8.jpg" />
+                <img />
+              </div>
+            </article>
+          </div>
+        </ProfileBody>
+      </ProfileContainer>
+    </div>
   );
 }
 
